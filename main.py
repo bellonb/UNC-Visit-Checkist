f242d3cdf3331f51b4e7be0890c48455542c4d81
bellonb <bellowen@hotmail.com>
2024-11-07 22:33:26 -0500

    quickstart changes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import tensorflow as tensorf
-import matplotlib.pyplot as plt
-import os
+# quickstart import
+# https://keras.io/keras_cv/
 
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"  # Or "jax" or "torch"!
+
+import tensorflow as tf
+import keras_cv
+import tensorflow_datasets as tfds
+import keras
